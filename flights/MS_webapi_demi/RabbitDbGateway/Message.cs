c0c41b6f438e1cff83192b0239d51527f745b0c9
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-05-28 14:25:41 +0300

    Add files via upload

---
+++ flights/MS_webapi_demi/RabbitDbGateway/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitConsumerCore
+{
+    class Message
+    {
+        public string queue_result_name { get; set; }
+        public string corr_id { get; set; }
+        public string sp_name { get; set; }
+        public string db_rows { get; set; }
+    }
+}
